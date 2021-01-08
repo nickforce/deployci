a9612fdf243ce5526ecea30a243f8cc657e6a0ff
ShaniEnginer <engusman8279@gmail.com>
2021-01-08 23:46:01 +0500

    changed token authentication from JWT to rest_frame_work Token Authentication

--- core/c_integration/urls.py
+++ core/c_integration/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path  , include
+# from rest_framework_jwt.views import obtain_jwt_token
+# from rest_framework_jwt.views import verify_jwt_token
+# from rest_framework_jwt.views import refresh_jwt_token
 
 
 
@@ -11,6 +14,10 @@ urlpatterns = [
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('c_integration_app.api.urls')),
     
+
+    # path('api-token-auth/', obtain_jwt_token),
+    # path('api-token-refresh/', refresh_jwt_token),
+    # path('api-token-verify/', verify_jwt_token),
 
 ]
 
