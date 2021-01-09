6791ed9f77f4df7de55d80f2b51ab5d7c39d6882
shanienginer <engusman8279@gmail.com>
2021-01-09 15:32:38 +0500

    added form data and faked post requesting

--- frontend/src/components/Models_content/JobsForm.jsx
+++ frontend/src/components/Models_content/JobsForm.jsx
@@ -1,4 +1,6 @@
 import { Form, Input, Button, Select } from "antd";
+import React, { useState } from "react";
+import axios from "axios";
 
 const { Option } = Select;
 
@@ -11,16 +13,37 @@ const tailLayout = {
 };
 
 const JobsForm = () => {
+  const [form] = Form.useForm();
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [relation, setRelation] = useState("");
+  const handleSubmit = (e) => {
+    const data = { name: name, type: type, relation: relation };
+    axios
+      .post(`https://jsonplaceholder.typicode.com/posts`, data)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+
+    form.resetFields();
+  };
+  const handleSelect = (e) => {
+    setRelation(e);
+  };
   return (
-    <Form {...layout} name="control-hooks">
+    <Form
+      onSubmitCapture={handleSubmit}
+      {...layout}
+      form={form}
+      name="control-hooks"
+    >
       <Form.Item name="Name" label="Name" rules={[{ required: true }]}>
-        <Input />
+        <Input onChange={(e) => setName(e.target.value)} />
       </Form.Item>
       <Form.Item name="Type" label="Type" rules={[{ required: true }]}>
-        <Input />
+        <Input onChange={(e) => setType(e.target.value)} />
       </Form.Item>
       <Form.Item name="Relation" label="Relation" rules={[{ required: true }]}>
-        <Select placeholder="Select a option ">
+        <Select onChange={handleSelect} placeholder="Select a option ">
           <Option value="relation 1">Relation 1</Option>
           <Option value="relation 2">Relation 2</Option>
           <Option value="relation 3">Relation 3</Option>
--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,7 +1,7 @@
 import { Form, Input, Button, Select } from "antd";
 import { Component } from "react";
 import github from "../assests/github.png";
-// import Github from "../assests/github.pnj";
+import axios from "axios";
 const layout = {
   labelCol: { span: 6 },
   wrapperCol: { span: 10 },
@@ -11,13 +11,30 @@ const tailLayout = {
 };
 
 class SignUp extends Component {
+  state = {
+    name: "",
+    password: "",
+  };
+  handleSubmit = () => {
+    const data = this.state;
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", data)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
   render() {
     return (
       <div className="container">
         <div className="center">
-          <Form {...layout} name="control-hooks">
+          <Form
+            onSubmitCapture={this.handleSubmit}
+            {...layout}
+            name="control-hooks"
+          >
             <Form.Item name="Email" label="Email" rules={[{ required: true }]}>
-              <Input />
+              <Input
+                onChange={(e) => this.setState({ name: e.target.value })}
+              />
             </Form.Item>
             <Form.Item
               label="Password"
@@ -26,14 +43,16 @@ class SignUp extends Component {
                 { required: true, message: "Please input your password!" },
               ]}
             >
-              <Input.Password />
+              <Input.Password
+                onChange={(e) => this.setState({ password: e.target.value })}
+              />
             </Form.Item>
             <Form.Item {...tailLayout}>
               <Button
                 id="github"
                 type="Default"
                 shape="round"
-                htmlType="submit"
+                htmlType="button"
               >
                 <a href="https://github.com/login/oauth/authorize?&client_id=Iv1.f50a9e441abc37c0">
                   Sign In with Github <i class="fab fa-github"></i>
