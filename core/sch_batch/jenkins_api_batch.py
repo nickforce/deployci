c9338723632b487409ce99960c5b1c70f432fe09
nickforce <nickforce@MacBook-Air.local>
2021-01-23 12:08:29 -0600

    fixed scheduling interface

--- core/sch_batch/jenkins_api_batch.py
+++ core/sch_batch/jenkins_api_batch.py
@@ -1,6 +1,6 @@
 import jenkins
 
-def jenkins_version(request):
+def jenkins_version():
     print('jenkins')
     print('jenkins')
     server = jenkins.Jenkins('http://ec2-18-222-146-212.us-east-2.compute.amazonaws.com:8080/', username='nick-admin-aws', password='upHCjJTCe8E7')
